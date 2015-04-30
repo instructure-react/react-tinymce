bed14e3b5d9bd667a8342a36f6d1fe0aa652506e
mzabriskie <mzabriskie@gmail.com>
2015-04-30 16:34:44 -0600

    Stubbing in tests

---
+++ specs/TinyMCE.spec.js
@@ -0,0 +1,7 @@
+require('./helper');
+
+describe('react-tinymce', function () {
+  it('should render', function () {
+    equal(true, true);
+  });
+});
---
+++ specs/helper.js
@@ -0,0 +1,9 @@
+React = require('react/addons');
+TestUtils = React.addons.TestUtils;
+TinyMCE = require('../lib/main');
+
+assert = require('assert');
+ok = assert.ok;
+equal = assert.equal;
+strictEqual = assert.strictEqual;
+throws = assert.throws;
