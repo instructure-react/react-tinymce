4bbabea90c8d05dbd88329eeb6b3f2842ef57913
mzabriskie <mzabriskie@gmail.com>
2015-06-16 16:52:20 -0600

    Updating rackt-cli

--- specs/TinyMCE.spec.js
+++ specs/TinyMCE.spec.js
@@ -1,4 +1,7 @@
-require('./helper');
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+var TinyMCE = require('../lib/main');
+var {ok, equal, strictEqual, throws} = require('assert');
 
 describe('react-tinymce', function () {
   it('should render', function () {
